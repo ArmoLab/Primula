eb80a5c8df11a46cd05158278d6f37a8ac7719a9
kobe-koto <91669733+kobe-koto@users.noreply.github.com>
2023-02-10 13:59:18 +0800

    Initial commit

---
+++ src/components/header.vue
@@ -0,0 +1,148 @@
+<template>
+    <header>
+        <span class="title">Website (Preview)</span>
+        <nav class="menu">
+            <a 
+                v-for="item in navigation" 
+                :key="item.name" 
+                :href="item.href" 
+                :class="[item.current ? 'current' : '']" 
+            >
+            {{ item.name }}
+        </a>
+        </nav>
+    </header>
+</template>
+  
+<script setup>
+    const navigation = [
+        { name: 'Home',       current: true,     href: 'javascript:void(0)'},
+        { name: 'Yggdrasil',  current: false,    href: 'https://skin.qianyiovo.com/auth/register'},
+        { name: 'Discord',    current: false,    href: 'https://discord.gg/Yd7sxzAeQv'},
+        { name: 'Settings',   current: false,    href: 'javascript:alert("沒有寫捏")'},
+    ]
+</script>
+
+<style scoped>
+    header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+
+        -webkit-backdrop-filter: blur(15px);
+        backdrop-filter: blur(15px);
+
+        background-color: rgba(31, 31, 31, 0.75);
+        padding: 15px 0;
+        color: #f8f8ff;
+    }
+
+    span.title {
+        margin-left: 15px;
+        letter-spacing: 2px;
+        opacity: 0;
+        transition: opacity,letter-spacing ease 0.55s;
+    }
+    nav.menu {
+        opacity: 0;
+        transition: opacity ease 0.35s;
+        margin-right: 15px;
+        float: right;
+    }
+
+    body.loaded span.title {
+        letter-spacing: 0px;
+        opacity: 1;
+    }
+    body.loaded nav.menu {
+        opacity: 1;
+    }
+
+    nav.menu > a:not(:last-child) {
+        margin-right: 10px;
+    }
+    nav.menu > a {
+        color: #f8f8f8;
+        text-decoration: none;
+        display: inline-block;
+    }
+    nav.menu > a::after {
+        content: "";
+        width: 0px;
+        height: 1px;
+        display: block;
+        background: #f8f8ff;
+        margin: 0 0 0 auto;
+
+        -webkit-transition: width 0.3s ease;
+        transition: width 0.3s ease;
+    }
+    nav.menu > a:hover::after {
+        width: 100%;
+        margin: 0 auto 0 0;
+    }
+
+    nav.menu > a.current {
+        margin-bottom: -10px;
+    }
+    nav.menu > a.current::after {
+        position: relative;
+        top: -10px;
+        left: 5px;
+        z-index: -1;
+
+        width: 100%;
+        height: 10px;
+        border-radius: 5px;
+        opacity: .35;
+        background-color: #00ffff;
+    }
+    nav.menu > a.current:hover::after {
+        -webkit-animation: shake 0.3s ease;
+        animation: shake .45s linear;
+    }
+
+    @-webkit-keyframes shake {
+        0%, 100% {
+            -webkit-transform: translateX(0px);
+        }
+        33% {
+            -webkit-transform: translateX(-1px);
+        }
+        66% {
+            -webkit-transform: translateX(1px);
+        }
+    }
+    @keyframes shake {
+        0%, 100% {
+            transform: translateX(0px);
+        }
+        33% {
+            transform: translateX(-1px);
+        }
+        66% {
+            transform: translateX(1px);
+        }
+    }
+
+
+    @media (max-width: 600px)  {
+        header {
+            text-align: center;
+        }
+        span.title, nav.menu {
+            display: block;
+            width: 100%;
+            float: none;
+        }
+        span.title {
+            margin-left: 0;
+        }
+        nav.menu {
+            margin-right: 0;
+            margin-top: 5px;
+        }
+    }
+
+</style>
---
+++ src/components/introduction.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="IntroductionContainer">
+        <div class="Introduction">
+            <h2>Introduction</h2>
+            <button class="bg-transparent">Getting Started ></button>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+    div.IntroductionContainer {
+        /* size */
+        min-height: 400px;
+        width: 100%;
+
+        /* background */
+        background-color: #1f1f1f;
+        background-position: center;
+        background-size: cover;
+        background-image: url("/images/2022-02-20-15-40-05_1080_commpressed.png");
+
+        /* item center */
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: flex;
+
+        justify-content: center;
+        align-items: center;
+    } 
+    div.Introduction {
+        text-align: center;
+    }
+    button {
+        border-style: solid;
+        border-color: #f8f8ff;
+        border-width: 2px;
+        border-radius: 5px;
+
+        padding: 15px 10px;
+
+        color: #f8f8ff;
+        font-size: medium;
+        font-weight: bold;
+    }
+    button.bg-transparent {
+        background-color: rgba(0,0,0,0);
+    }
+</style>
+
+
+
+<!--   <script setup>
+</script> -->
